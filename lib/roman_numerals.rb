caa695a926d8c32e5b8e7d59a19df93183f2a332
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-01-07 15:08:17 +0000

    Extracts roman numerals into an array

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,12 +1,14 @@
 class RomanNumerals
   DECIMALS = [0, 1, 5]
+ ROMANS = ["", "I", "V"] 
   def translate(numeral)
-    if numeral == "I"
-      decimal = DECIMALS[1]
-    elsif numeral == "V"
-      decimal = DECIMALS[2]
-    elsif numeral == ""
+ 
+    if numeral == ROMANS[0]
       decimal = DECIMALS[0]
+    elsif numeral == ROMANS[1]
+      decimal = DECIMALS[1]
+    elsif numeral == ROMANS[2]
+      decimal = DECIMALS[2]
     end
     decimal
   end
